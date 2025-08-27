70beadee69cf765eb2ceddb2da3006db3ae24b4d
Siddhi Bajpai <bajpaisiddhi20@gmail.com>
2025-08-27 23:36:42 +0530

    Create exp_9(3).cpp

---
+++ exp_9(3).cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  int a=10;
+  int* aptr=&a;
+  cout<<*aptr<<endl;
+  *aptr=20;
+  cout<<a<<endl;
+  int arr[]={10,20,30};
+  cout<<*arr<<endl;
+    return 0;
+}
+
+#Output:
+#10
+#20
+#10
